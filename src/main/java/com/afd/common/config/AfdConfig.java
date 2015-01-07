adf82d999e00f300c06d4d3b3d5c32b4296dec79
xuzunyuan <xuzunyuan@yiwang.com>
2015-01-07 17:34:40 +0800

    v

---
+++ src/main/java/com/afd/common/config/AfdConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c)2013-2014 by afd. All rights reserved.
+ * 
+ */
+package com.afd.common.config;
+
+/**
+ * 通用设置均在此处定义
+ * 
+ * @author xuzunyuan
+ * @date 2013年12月25日
+ */
+public final class AfdConfig {
+	private AfdConfig() {
+	}
+
+	public static final String charset = "utf-8"; // 通用编码
+}
