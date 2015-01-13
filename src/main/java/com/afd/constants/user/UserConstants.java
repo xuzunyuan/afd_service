d2ea82bc5f72760ec7f2c15b0c1c2cc100b25a97
liubo <liubo@yiwang.com>
2015-01-13 12:24:33 +0800

    v

---
+++ src/main/java/com/afd/constants/user/UserConstants.java
@@ -0,0 +1,24 @@
+package com.afd.constants.user;
+
+public interface UserConstants {
+	/******************geo常量***************************/
+	/**
+	 * 缓存中省份的key
+	 */
+	public static final String GEO_PROVINCE = "provinces";
+	
+	/**
+	 * 省份在geo表中的level
+	 */
+	public static final String GEO_LEVEL_PROVINCE = "1";
+	
+	/**
+	 * 缓存中地址叶子节点的前缀
+	 */
+	public static final String GEO_BY_FID = "geoFid_";
+	
+	/**
+	 * 缓存中地址前缀
+	 */
+	public static final String GEO = "geo_";
+}
--- src/main/java/com/afd/service/user/IGeoService.java
+++ src/main/java/com/afd/service/user/IGeoService.java
@@ -9,5 +9,19 @@ public interface IGeoService {
 	 * 获取省级地址表
 	 * @return
 	 */
-	public List<Geo> getGeoLevelOne();
+	public List<Geo> getGeoProvince();
+	
+	/**
+	 * 根据父地址id查询子节点
+	 * @param fid
+	 * @return
+	 */
+	public List<Geo> getGeoByFId(long fid);
+	
+	/**
+	 * 根据geo的id获取geo
+	 * @param id
+	 * @return
+	 */
+	public Geo getGeoById(int id);
 }
