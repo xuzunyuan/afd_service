d1ecb10cc652bc9868240c8f26461dd853506925
liubo <liubo@yiwang.com>
2015-01-19 15:00:44 +0800

    v

--- src/main/java/com/afd/model/order/OrderItem.java
+++ src/main/java/com/afd/model/order/OrderItem.java
@@ -6,191 +6,193 @@ import java.math.BigDecimal;
 public class OrderItem implements Serializable{
     private Long orderItemId;
 
-    private Long orderId;
+	private Integer orderId;
 
-    private String orderCode;
+	private String orderCode;
 
-    private Integer skuId;
+	private Integer skuId;
 
-    private Integer prodId;
+	private Integer prodId;
 
-    private String prodCode;
+	private String prodCode;
 
-    private String skuCode;
+	private String skuCode;
 
-    private String prodSpecId;
+	private String prodSpecId;
 
-    private String prodSpecName;
+	private String prodSpecName;
 
-    private String prodTitle;
+	private String prodTitle;
 
-    private Long bcId;
+	private Long bcId;
 
-    private Long sellerId;
+	private Long sellerId;
 
-    private BigDecimal salePrice;
+	private BigDecimal salePrice;
 
-    private BigDecimal transPrice;
+	private BigDecimal transPrice;
 
-    private Short number;
+	private Long number;
 
-    private String status;
+	private String status;
 
-    private String isComment;
+	private String isComment;
 
-    private Long brandShowId;
+	private Long brandShowId;
 
-    private String brandShowTitle;
+	private String brandShowTitle;
 
-    public Long getOrderItemId() {
-        return orderItemId;
-    }
+	public Long getOrderItemId() {
+		return orderItemId;
+	}
 
-    public void setOrderItemId(Long orderItemId) {
-        this.orderItemId = orderItemId;
-    }
+	public void setOrderItemId(Long orderItemId) {
+		this.orderItemId = orderItemId;
+	}
 
-    public Long getOrderId() {
-        return orderId;
-    }
+	public Integer getOrderId() {
+		return orderId;
+	}
 
-    public void setOrderId(Long orderId) {
-        this.orderId = orderId;
-    }
+	public void setOrderId(Integer orderId) {
+		this.orderId = orderId;
+	}
 
-    public String getOrderCode() {
-        return orderCode;
-    }
+	public String getOrderCode() {
+		return orderCode;
+	}
 
-    public void setOrderCode(String orderCode) {
-        this.orderCode = orderCode == null ? null : orderCode.trim();
-    }
+	public void setOrderCode(String orderCode) {
+		this.orderCode = orderCode == null ? null : orderCode.trim();
+	}
 
-    public Integer getSkuId() {
-        return skuId;
-    }
+	public Integer getSkuId() {
+		return skuId;
+	}
 
-    public void setSkuId(Integer skuId) {
-        this.skuId = skuId;
-    }
+	public void setSkuId(Integer skuId) {
+		this.skuId = skuId;
+	}
 
-    public Integer getProdId() {
-        return prodId;
-    }
+	public Integer getProdId() {
+		return prodId;
+	}
 
-    public void setProdId(Integer prodId) {
-        this.prodId = prodId;
-    }
+	public void setProdId(Integer prodId) {
+		this.prodId = prodId;
+	}
 
-    public String getProdCode() {
-        return prodCode;
-    }
+	public String getProdCode() {
+		return prodCode;
+	}
 
-    public void setProdCode(String prodCode) {
-        this.prodCode = prodCode == null ? null : prodCode.trim();
-    }
+	public void setProdCode(String prodCode) {
+		this.prodCode = prodCode == null ? null : prodCode.trim();
+	}
 
-    public String getSkuCode() {
-        return skuCode;
-    }
+	public String getSkuCode() {
+		return skuCode;
+	}
 
-    public void setSkuCode(String skuCode) {
-        this.skuCode = skuCode == null ? null : skuCode.trim();
-    }
+	public void setSkuCode(String skuCode) {
+		this.skuCode = skuCode == null ? null : skuCode.trim();
+	}
 
-    public String getProdSpecId() {
-        return prodSpecId;
-    }
+	public String getProdSpecId() {
+		return prodSpecId;
+	}
 
-    public void setProdSpecId(String prodSpecId) {
-        this.prodSpecId = prodSpecId == null ? null : prodSpecId.trim();
-    }
+	public void setProdSpecId(String prodSpecId) {
+		this.prodSpecId = prodSpecId == null ? null : prodSpecId.trim();
+	}
 
-    public String getProdSpecName() {
-        return prodSpecName;
-    }
+	public String getProdSpecName() {
+		return prodSpecName;
+	}
 
-    public void setProdSpecName(String prodSpecName) {
-        this.prodSpecName = prodSpecName == null ? null : prodSpecName.trim();
-    }
+	public void setProdSpecName(String prodSpecName) {
+		this.prodSpecName = prodSpecName == null ? null : prodSpecName.trim();
+	}
 
-    public String getProdTitle() {
-        return prodTitle;
-    }
+	public String getProdTitle() {
+		return prodTitle;
+	}
 
-    public void setProdTitle(String prodTitle) {
-        this.prodTitle = prodTitle == null ? null : prodTitle.trim();
-    }
+	public void setProdTitle(String prodTitle) {
+		this.prodTitle = prodTitle == null ? null : prodTitle.trim();
+	}
 
-    public Long getBcId() {
-        return bcId;
-    }
+	public Long getBcId() {
+		return bcId;
+	}
 
-    public void setBcId(Long bcId) {
-        this.bcId = bcId;
-    }
+	public void setBcId(Long bcId) {
+		this.bcId = bcId;
+	}
 
-    public Long getSellerId() {
-        return sellerId;
-    }
+	public Long getSellerId() {
+		return sellerId;
+	}
 
-    public void setSellerId(Long sellerId) {
-        this.sellerId = sellerId;
-    }
+	public void setSellerId(Long sellerId) {
+		this.sellerId = sellerId;
+	}
 
-    public BigDecimal getSalePrice() {
-        return salePrice;
-    }
+	public BigDecimal getSalePrice() {
+		return salePrice;
+	}
 
-    public void setSalePrice(BigDecimal salePrice) {
-        this.salePrice = salePrice;
-    }
+	public void setSalePrice(BigDecimal salePrice) {
+		this.salePrice = salePrice;
+	}
 
-    public BigDecimal getTransPrice() {
-        return transPrice;
-    }
+	public BigDecimal getTransPrice() {
+		return transPrice;
+	}
 
-    public void setTransPrice(BigDecimal transPrice) {
-        this.transPrice = transPrice;
-    }
+	public void setTransPrice(BigDecimal transPrice) {
+		this.transPrice = transPrice;
+	}
 
-    public Short getNumber() {
-        return number;
-    }
+	public Long getNumber() {
+		return number;
+	}
 
-    public void setNumber(Short number) {
-        this.number = number;
-    }
+	public void setNumber(Long number) {
+		this.number = number;
+	}
 
-    public String getStatus() {
-        return status;
-    }
+	public String getStatus() {
+		return status;
+	}
 
-    public void setStatus(String status) {
-        this.status = status == null ? null : status.trim();
-    }
+	public void setStatus(String status) {
+		this.status = status == null ? null : status.trim();
+	}
 
-    public String getIsComment() {
-        return isComment;
-    }
+	public String getIsComment() {
+		return isComment;
+	}
 
-    public void setIsComment(String isComment) {
-        this.isComment = isComment == null ? null : isComment.trim();
-    }
+	public void setIsComment(String isComment) {
+		this.isComment = isComment == null ? null : isComment.trim();
+	}
 
-    public Long getBrandShowId() {
-        return brandShowId;
-    }
+	public Long getBrandShowId() {
+		return brandShowId;
+	}
 
-    public void setBrandShowId(Long brandShowId) {
-        this.brandShowId = brandShowId;
-    }
+	public void setBrandShowId(Long brandShowId) {
+		this.brandShowId = brandShowId;
+	}
 
-    public String getBrandShowTitle() {
-        return brandShowTitle;
-    }
+	public String getBrandShowTitle() {
+		return brandShowTitle;
+	}
 
-    public void setBrandShowTitle(String brandShowTitle) {
-        this.brandShowTitle = brandShowTitle == null ? null : brandShowTitle.trim();
-    }
+	public void setBrandShowTitle(String brandShowTitle) {
+		this.brandShowTitle = brandShowTitle == null ? null : brandShowTitle
+				.trim();
+	}
+
 }
