dda5c3593b3704d4ef4388b9b83867ff4170c6ef
xuzunyuan <xuzunyuan@yiwang.com>
2015-01-19 16:34:20 +0800

    v

--- src/main/java/com/afd/model/img/ImgResource.java
+++ src/main/java/com/afd/model/img/ImgResource.java
@@ -1,7 +1,6 @@
 package com.afd.model.img;
 
 import java.io.Serializable;
-import java.util.Date;
 
 public class ImgResource implements Serializable {
 
@@ -20,20 +19,14 @@ public class ImgResource implements Serializable {
 	/* 分配给图片的ID，使用ID下载图片 */
 	private String id;
 
-	/* 创建时间 */
-	private Date createTime;
-
-	/* 文件大小 */
-	private long size;
-
-	/* 文件名称 */
-	private String name;
-
 	/* 图片高度 */
 	private int height;
 
 	/* 图片宽度 */
 	private int width;
+
+	/* 文件类型 */
+	private String type;
 
 	public String getId() {
 		return id;
@@ -41,30 +34,6 @@ public class ImgResource implements Serializable {
 
 	public void setId(String id) {
 		this.id = id;
-	}
-
-	public Date getCreateTime() {
-		return createTime;
-	}
-
-	public void setCreateTime(Date createTime) {
-		this.createTime = createTime;
-	}
-
-	public long getSize() {
-		return size;
-	}
-
-	public void setSize(long size) {
-		this.size = size;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
 	}
 
 	public int getHeight() {
@@ -81,6 +50,14 @@ public class ImgResource implements Serializable {
 
 	public void setWidth(int width) {
 		this.width = width;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
 	}
 
 }
