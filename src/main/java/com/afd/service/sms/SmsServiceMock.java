e027aafd7f4f07c740df33ce31fb36797b6a52e4
xuzunyuan <xuzunyuan@yiwang.com>
2015-01-26 11:46:05 +0800

    v

--- src/main/java/com/afd/service/sms/SmsServiceMock.java
+++ src/main/java/com/afd/service/sms/SmsServiceMock.java
@@ -1,24 +1,37 @@
 package com.afd.service.sms;
 
+import org.apache.commons.lang.ArrayUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class SmsServiceMock implements ISmsService {
+	private static final Logger logger = LoggerFactory
+			.getLogger(SmsServiceMock.class);
 
 	@Override
 	public int sendSmsMultiEx(String[] mobile, String smsContent, String bizCode) {
+		logger.info("[mobile] " + ArrayUtils.toString(mobile)
+				+ " [smsContent] " + smsContent + " [bizCode] " + bizCode);
 		return 0;
 	}
 
 	@Override
 	public int sendSmsMulti(String[] mobile, String smsContent) {
+		logger.info("[mobile] " + ArrayUtils.toString(mobile)
+				+ " [smsContent] " + smsContent);
 		return 0;
 	}
 
 	@Override
 	public int sendSmsEx(String mobile, String smsContent, String bizCode) {
+		logger.info("[mobile] " + mobile + " [smsContent] " + smsContent
+				+ " [bizCode] " + bizCode);
 		return 0;
 	}
 
 	@Override
 	public int sendSms(String mobile, String smsContent) {
+		logger.info("[mobile] " + mobile + " [smsContent] " + smsContent);
 		return 0;
 	}
 
