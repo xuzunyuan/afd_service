327518858be4315b9db705e221ef74bd43e1473d
xuzunyuan <xuzunyuan@yiwang.com>
2015-01-26 15:49:08 +0800

    v

--- src/main/java/com/afd/service/seller/ISellerLoginService.java
+++ src/main/java/com/afd/service/seller/ISellerLoginService.java
@@ -4,8 +4,10 @@
  */
 package com.afd.service.seller;
 
+import java.io.Serializable;
 import java.util.Date;
 
+import com.afd.model.seller.Seller;
 import com.afd.model.seller.SellerLogin;
 
 /**
@@ -103,4 +105,45 @@ public interface ISellerLoginService {
 			String oldPassword);
 
 	public int changePassword(int loginId, String newPassword);
+
+	public static class SellerLoginValidate implements Serializable {
+		public static final int PASSED = 1; // 通过
+		public static final int NOT_EXIST = 0; // 账号不存在
+		public static final int INCORRECT_PASSWORD = -1; // 密码不正确
+		public static final int FREEZED = -2; // 冻结
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 5513716822016457467L;
+
+		private int validateStatus;
+		private SellerLogin sellerLogin;
+		private Seller seller;
+
+		public Seller getSeller() {
+			return seller;
+		}
+
+		public void setSeller(Seller seller) {
+			this.seller = seller;
+		}
+
+		public int getValidateStatus() {
+			return validateStatus;
+		}
+
+		public void setValidateStatus(int validateStatus) {
+			this.validateStatus = validateStatus;
+		}
+
+		public SellerLogin getSellerLogin() {
+			return sellerLogin;
+		}
+
+		public void setSellerLogin(SellerLogin sellerLogin) {
+			this.sellerLogin = sellerLogin;
+		}
+	}
+
 }
