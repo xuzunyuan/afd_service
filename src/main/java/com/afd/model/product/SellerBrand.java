99a64ea987bff6bb9f4047aaf8b7880fe0473544
xuzunyuan <xuzunyuan@yiwang.com>
2015-02-06 10:34:42 +0800

    v

--- src/main/java/com/afd/model/product/SellerBrand.java
+++ src/main/java/com/afd/model/product/SellerBrand.java
@@ -8,39 +8,39 @@ public class SellerBrand implements Serializable {
 
 	private Integer sellerBrandId;
 
-    private Integer sellerId;
+	private Integer sellerId;
 
-    private Integer brandId;
+	private Integer brandId;
 
-    private Date submitDate;
+	private Date submitDate;
 
-    private String status;
+	private String status;
 
-    private String auditStatus;
+	private String auditStatus;
 
-    private Date auditDate;
+	private Date auditDate;
 
-    private String auditByName;
+	private String auditByName;
 
-    private String auditContent;
+	private String auditContent;
 
-    private Date authStartDate;
+	private Date authStartDate;
 
-    private Date authEndDate;
+	private Date authEndDate;
 
-    private String categories;
+	private String categories;
 
-    private String trademarkCert;
+	private String trademarkCert;
 
-    private String otherCert;
+	private String otherCert;
 
-    private String authCert;
-    
-    private String loginName;
-    
-    private String co_name;
-    
-    public String getLoginName() {
+	private String authCert;
+
+	private String loginName;
+
+	private String co_name;
+
+	public String getLoginName() {
 		return loginName;
 	}
 
@@ -57,122 +57,154 @@ public class SellerBrand implements Serializable {
 	}
 
 	public Integer getSellerBrandId() {
-        return sellerBrandId;
-    }
+		return sellerBrandId;
+	}
 
-    public void setSellerBrandId(Integer sellerBrandId) {
-        this.sellerBrandId = sellerBrandId;
-    }
+	public void setSellerBrandId(Integer sellerBrandId) {
+		this.sellerBrandId = sellerBrandId;
+	}
 
-    public Integer getSellerId() {
-        return sellerId;
-    }
+	public Integer getSellerId() {
+		return sellerId;
+	}
 
-    public void setSellerId(Integer sellerId) {
-        this.sellerId = sellerId;
-    }
+	public void setSellerId(Integer sellerId) {
+		this.sellerId = sellerId;
+	}
 
-    public Integer getBrandId() {
-        return brandId;
-    }
+	public Integer getBrandId() {
+		return brandId;
+	}
 
-    public void setBrandId(Integer brandId) {
-        this.brandId = brandId;
-    }
+	public void setBrandId(Integer brandId) {
+		this.brandId = brandId;
+	}
 
-    public Date getSubmitDate() {
-        return submitDate;
-    }
+	public Date getSubmitDate() {
+		return submitDate;
+	}
 
-    public void setSubmitDate(Date submitDate) {
-        this.submitDate = submitDate;
-    }
+	public void setSubmitDate(Date submitDate) {
+		this.submitDate = submitDate;
+	}
 
-    public String getStatus() {
-        return status;
-    }
+	public String getStatus() {
+		return status;
+	}
 
-    public void setStatus(String status) {
-        this.status = status;
-    }
+	public void setStatus(String status) {
+		this.status = status;
+	}
 
-    public String getAuditStatus() {
-        return auditStatus;
-    }
+	public String getAuditStatus() {
+		return auditStatus;
+	}
 
-    public void setAuditStatus(String auditStatus) {
-        this.auditStatus = auditStatus;
-    }
+	public void setAuditStatus(String auditStatus) {
+		this.auditStatus = auditStatus;
+	}
 
-    public Date getAuditDate() {
-        return auditDate;
-    }
+	public Date getAuditDate() {
+		return auditDate;
+	}
 
-    public void setAuditDate(Date auditDate) {
-        this.auditDate = auditDate;
-    }
+	public void setAuditDate(Date auditDate) {
+		this.auditDate = auditDate;
+	}
 
-    public String getAuditByName() {
-        return auditByName;
-    }
+	public String getAuditByName() {
+		return auditByName;
+	}
 
-    public void setAuditByName(String auditByName) {
-        this.auditByName = auditByName;
-    }
+	public void setAuditByName(String auditByName) {
+		this.auditByName = auditByName;
+	}
 
-    public String getAuditContent() {
-        return auditContent;
-    }
+	public String getAuditContent() {
+		return auditContent;
+	}
 
-    public void setAuditContent(String auditContent) {
-        this.auditContent = auditContent;
-    }
+	public void setAuditContent(String auditContent) {
+		this.auditContent = auditContent;
+	}
 
-    public Date getAuthStartDate() {
-        return authStartDate;
-    }
+	public Date getAuthStartDate() {
+		return authStartDate;
+	}
 
-    public void setAuthStartDate(Date authStartDate) {
-        this.authStartDate = authStartDate;
-    }
+	public void setAuthStartDate(Date authStartDate) {
+		this.authStartDate = authStartDate;
+	}
 
-    public Date getAuthEndDate() {
-        return authEndDate;
-    }
+	public Date getAuthEndDate() {
+		return authEndDate;
+	}
 
-    public void setAuthEndDate(Date authEndDate) {
-        this.authEndDate = authEndDate;
-    }
+	public void setAuthEndDate(Date authEndDate) {
+		this.authEndDate = authEndDate;
+	}
 
-    public String getCategories() {
-        return categories;
-    }
+	public String getCategories() {
+		return categories;
+	}
 
-    public void setCategories(String categories) {
-        this.categories = categories;
-    }
+	public void setCategories(String categories) {
+		this.categories = categories;
+	}
 
-    public String getTrademarkCert() {
-        return trademarkCert;
-    }
+	public String getTrademarkCert() {
+		return trademarkCert;
+	}
 
-    public void setTrademarkCert(String trademarkCert) {
-        this.trademarkCert = trademarkCert;
-    }
+	public void setTrademarkCert(String trademarkCert) {
+		this.trademarkCert = trademarkCert;
+	}
 
-    public String getOtherCert() {
-        return otherCert;
-    }
+	public String getOtherCert() {
+		return otherCert;
+	}
 
-    public void setOtherCert(String otherCert) {
-        this.otherCert = otherCert;
-    }
+	public void setOtherCert(String otherCert) {
+		this.otherCert = otherCert;
+	}
 
-    public String getAuthCert() {
-        return authCert;
-    }
+	public String getAuthCert() {
+		return authCert;
+	}
 
-    public void setAuthCert(String authCert) {
-        this.authCert = authCert;
-    }
+	public void setAuthCert(String authCert) {
+		this.authCert = authCert;
+	}
+
+	// 扩展
+	private String brandName;
+
+	private String brandEname;
+
+	private String logoUrl;
+
+	public String getBrandName() {
+		return brandName;
+	}
+
+	public void setBrandName(String brandName) {
+		this.brandName = brandName;
+	}
+
+	public String getBrandEname() {
+		return brandEname;
+	}
+
+	public void setBrandEname(String brandEname) {
+		this.brandEname = brandEname;
+	}
+
+	public String getLogoUrl() {
+		return logoUrl;
+	}
+
+	public void setLogoUrl(String logoUrl) {
+		this.logoUrl = logoUrl;
+	}
+
 }
