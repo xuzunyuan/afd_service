4c9221c28977c1b407d7c2b9157dca035c978dd1
lgc <linguichun@yiwang.com>
2015-03-11 10:03:29 +0800

    增加退货地址服务方法

--- src/main/java/com/afd/service/seller/ISellerRetAddrService.java
+++ src/main/java/com/afd/service/seller/ISellerRetAddrService.java
@@ -12,10 +12,21 @@ import com.afd.model.seller.SellerRetAddress;
  * 卖家退货地址服务
  * 
  * @author xuzunyuan
- * @date 2015年2月28日
- */
+ * @date 2015年2月28日 */
 public interface ISellerRetAddrService {
+	int deleteSellerRetAddressById(Integer sRAId);
+	
+	int insertSellerRetAddress(SellerRetAddress retAddress);
+	
+	int updateSellerRetAddressById(SellerRetAddress retAddress);
+	
 	public List<SellerRetAddress> getValidAddrListOfSeller(int sellerId);
 
 	public SellerRetAddress getAddrById(int id);
+	
+	/**
+	 * @param sRAId
+	 * @return 设置为默认收货地址
+	 */
+	public int setIsDefault(Integer sRAId, Integer sellerId);
 }
